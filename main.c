6171c5d165f13c0953741feb015c6f5e85d32347
Luis Claudio De Carvalho Frade <luide-ca@c1r2p2.42sp.org.br>
2025-01-27 11:20:39 -0300

    updating main and deleting tester

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 18:56:44 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/01/22 15:30:38 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/01/24 15:04:41 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,14 +53,14 @@ int	main(int argc, char **argv)
 {
 	t_elem	*lsta1;
 	t_elem	*lstb1;
-	t_elem	*lsta2;
-	t_elem	*lstb2;
+	// t_elem	*lsta2;
+	// t_elem	*lstb2;
 	int		val_res;
 
 	lsta1 = NULL;
 	lstb1 = NULL;
-	lsta2 = NULL;
-	lstb2 = NULL;
+	// lsta2 = NULL;
+	// lstb2 = NULL;
 
 	val_res = input_validator(argc, argv);
 	if (val_res == 0)
@@ -69,23 +69,23 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	lsta1 = stack_builder(argc, argv);
-	lsta2 = stack_builder(argc, argv);
-	if ((!lsta1 || lsta1 == 0) && (!lsta2 || lsta2 == 0))
+	// lsta2 = stack_builder(argc, argv);
+	if ((!lsta1 || lsta1 == 0) /*&& (!lsta2 || lsta2 == 0)*/)
 	{
 		write(2, "Error\n", 6);
 		return (0);
 	}
 	update_pos_n_target_pos(&lsta1, &lstb1);
-	movement_result(lsta1, lstb1);
+	//movement_result(lsta1, lstb1);
 	printf("==radix_algo==\n");
 	radix_algo(&lsta1, &lstb1);
 	movement_result(lsta1, lstb1);
-	printf("==mia_algo==\n");
-	mia_algo(&lsta2, &lstb2);
-	movement_result(lsta2, lstb2);
+	//printf("==mia_algo==\n");
+	//mia_algo(&lsta2, &lstb2);
+	//movement_result(lsta2, lstb2);
 	free_lst(lsta1);
 	free_lst(lstb1);
-	free_lst(lsta2);
-	free_lst(lstb2);
+	//free_lst(lsta2);
+	//free_lst(lstb2);
 	return (0);
 }
