74a501df32626d058607c972689a7e065e0dd4a8
Luis Claudio De Carvalho Frade <luide-ca@c1r2p2.42sp.org.br>
2025-01-27 12:25:17 -0300

    fixed aggregated movements

--- movements_aggregated.c
+++ movements_aggregated.c
@@ -6,7 +6,7 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 16:48:23 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/01/15 16:51:31 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/01/27 12:24:34 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,73 @@
 
 void	ss(t_elem **lst1, t_elem **lst2)
 {
-	if (lst1)
-		sx(lst1);
-	if (lst2)
-		sx(lst2);
+	t_elem	*tmp_head;
+	t_elem	*tmp_swap;
+	t_elem	*tmp_third;
+
+	if (ft_lstsize(*lst1) < 2 && ft_lstsize(*lst2) < 2)
+		return ;
+	tmp_head = *lst1;
+	tmp_swap = tmp_head->next_number;
+	tmp_third = tmp_swap->next_number;
+	tmp_head->next_number = tmp_third;
+	*lst1 = tmp_swap;
+	tmp_swap->next_number = tmp_head;
+	tmp_head = *lst2;
+	tmp_swap = tmp_head->next_number;
+	tmp_third = tmp_swap->next_number;
+	tmp_head->next_number = tmp_third;
+	*lst2 = tmp_swap;
+	tmp_swap->next_number = tmp_head;
+	write(1, "ss\n", 3);
 }
 
 void	rr(t_elem **lst1, t_elem **lst2)
 {
-	if (lst1)
-		rx(lst1);
-	if (lst2)
-		rx(lst2);
+	t_elem	*tmp_head;
+	t_elem	*new_top;
+	t_elem	*tmp_last;
+
+	if (ft_lstsize(*lst1) < 2 && ft_lstsize(*lst2) < 2)
+		return ;
+	tmp_head = *lst1;
+	new_top = tmp_head->next_number;
+	tmp_last = ft_lstlast(*lst1);
+	tmp_last->next_number = tmp_head;
+	tmp_head->next_number = NULL;
+	*lst1 = new_top;
+	tmp_head = *lst2;
+	new_top = tmp_head->next_number;
+	tmp_last = ft_lstlast(*lst2);
+	tmp_last->next_number = tmp_head;
+	tmp_head->next_number = NULL;
+	*lst2 = new_top;
+	write(1, "rr\n", 3);
 }
 
 void	rrr(t_elem **lst1, t_elem **lst2)
 {
-	if (lst1)
-		rrx(lst1);
-	if (lst2)
-		rrx(lst2);
+	t_elem	*new_top;
+	t_elem	*new_second;
+	t_elem	*new_last;
+
+	if (ft_lstsize(*lst1) < 2 && ft_lstsize(*lst2) < 2)
+		return ;
+	new_second = *lst1;
+	new_top = ft_lstlast(*lst1);
+	new_last = *lst1;
+	while (new_last->next_number != new_top)
+		new_last = new_last->next_number;
+	new_last->next_number = NULL;
+	new_top->next_number = new_second;
+	*lst1 = new_top;
+	new_second = *lst2;
+	new_top = ft_lstlast(*lst2);
+	new_last = *lst2;
+	while (new_last->next_number != new_top)
+		new_last = new_last->next_number;
+	new_last->next_number = NULL;
+	new_top->next_number = new_second;
+	*lst2 = new_top;
+	write(1, "rrr\n", 4);
 }
