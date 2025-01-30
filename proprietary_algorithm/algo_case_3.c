24713ca8c42ac203f5fc09e14bfb36ff36384df5
Luis Claudio De Carvalho Frade <luide-ca@c1r2p2.42sp.org.br>
2025-01-29 23:10:46 -0300

    final tunes on push_swap

--- proprietary_algorithm/algo_case_3.c
+++ proprietary_algorithm/algo_case_3.c
@@ -6,7 +6,7 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:20:12 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/01/29 17:49:18 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/01/29 23:02:29 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,19 +99,34 @@ void	lower_cost_to_top(t_elem **lst)
 void	perform_final_ra_or_rra(t_elem **lst)
 {
 	t_elem	*smallest_node;
+	int		lst_size;
+	int		pos;
 
-	smallest_node = get_smallest_node(lst);	
+	smallest_node = get_smallest_node(lst);
+	pos = smallest_node->position;
+	lst_size = ft_lstsize(*lst);
 	if (smallest_node->is_above_center == 1)
-		rx(lst);
+	{
+		while (pos != 0)
+		{
+			rx(lst);
+			pos--;
+		}
+	}
 	else if (smallest_node->is_above_center == 0)
-		rrx(lst);
+	{
+		while (pos != 0)
+		{
+			rrx(lst);
+			pos = pos - (lst_size + 1);
+		}
+	}
 }
 
 void	algo_case_3(t_elem **lst1, t_elem **lst2)
 {
-	t_elem	*smallest_node;
-	t_elem	*head;
 	int		lst_size2;
+	t_elem	*current;
 
 	move_elems_to_b(lst1, lst2);
 	if (check_ordered(*lst1) == 0)
@@ -122,17 +137,18 @@ void	algo_case_3(t_elem **lst1, t_elem **lst2)
 		update_pos(lst1);
 		update_pos(lst2);
 		update_target_pos(lst1, lst2);
-		printf("=== cost cal===\n");
 		cal_cost(lst1, lst2);
+		current = *lst2;
+		printf("\n stack a\n\n");
+		while (current)
+		{
+			printf("[%c]no[%d]pos:%d cost:%d\n", current->stack_letter, current->index, current->position, current->cost);
+			current = current->next_number;
+		}
 		lower_cost_to_top(lst2);
 		px(lst2, lst1);
 		lst_size2 = ft_lstsize(*lst2);
 	}
 	update_pos(lst1);
-	head = *lst1;
-	smallest_node = get_smallest_node(lst1);
-	if (!head || !smallest_node)
-		return ;
-	while (head != smallest_node)
-		perform_final_ra_or_rra(lst1);
+	perform_final_ra_or_rra(lst1);
 }
